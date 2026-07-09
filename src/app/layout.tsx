306ed47a20289c8fd5b68e97b706c2a40e392bd1
Muhammad Junaid <muhammad@192.168.0.228>
2026-07-09 10:57:22 +0500

    Add Google Analytics (G-DBMFQQQRTM)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,18 @@ export default function RootLayout({
             })(window, document, "clarity", "script", "xjli5b1ql6");
           `}
         </Script>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-DBMFQQQRTM"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-DBMFQQQRTM');
+          `}
+        </Script>
       </body>
     </html>
   );
